de0ed718567840cb884c20ce5a8c096ece2f1c91
Marvarro <devmarvarro@gmail.com>
2023-05-17 03:49:22 -0300

    Exercicios aula 15/05/2023

---
+++ aula-15-05-23/01-exercicios.js
@@ -0,0 +1,27 @@
+
+/**
+ * String - '' "" `` - Cadeia de caracter
+ */
+
+var nome = "Marcelo"
+var idade = 43
+var cidade = 'São Paulo'
+var uf = 'SP'
+
+var informacoes2 = `Meu nome é ${nome}, tenho ${idade} anos, moro em ${cidade} - ${uf} e trabalho como desenvolvedor`
+console.log(informacoes2)
+
+
+const email = "mdenavarros@gmail.com"
+const senha = "123456789"
+
+if(email == "mdenavarros@gmail.com" && senha == "123456789"){
+    console.log("Usuário autenticado")
+} else {
+    console.log("login e/ou senha incorretos")
+}
+
+
+
+
+
